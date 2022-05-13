25dc11203277279e337500a71e8b5cf4277970b2
awbwd4 <awbwd4@gmail.com>
2022-05-14 01:13:40 +0900

    22.05.13 bfs_maze

--- dfs_bfs/bfs_maze.py
+++ dfs_bfs/bfs_maze.py
@@ -8,10 +8,12 @@ for i in range(n):
     graph.append(list(map(int, input().split())))
 
 # 방문여부
-visited = [[False] * m] * n
+# visited = [[False] * m] * n ##이경우 n개의 [[False]*m]은 모두 같은 배열로 인식됨
+visited = [[False] * m for _ in range(n)]
+
 # 방향
-da = [0, 0, -1, 1]
-db = [-1, 1, 0, 0]
+da = [-1, 1, 0, 0]
+db = [0, 0, -1, 1]
 
 # queue = deque()
 # queue.append((0, 0))
@@ -30,24 +32,30 @@ def bfs():
     # queue에 첫 값 넣기
     queue.append((0, 0))
 
+    print(visited)
+    print("==================")
+
     # 큐에 값이 있을 경우
     while queue:
 
         a, b = queue.popleft()
         print("이동기준좌표 [%d %d]" % (a, b))
         visited[a][b] = True  # 방문여부 체크
+        print(visited)
 
         for i in range(4):  # 4방향 탐색
             na = a + da[i]
             nb = b + db[i]
-
-            print(na, " ", nb)
+            print("  ", na, " ", nb)
             # 이동하지 않는 조건
             if graph[na][nb] == 0:
+                print("         1.필터링된 na nb : ", na, " ", nb)
                 continue
-            if na < 0 or nb < 0 or na >= n or nb >= m:
+            if na < 0 or na >= n or nb < 0 or nb >= m:
+                print("         2.필터링된 na nb : ", na, " ", nb)
                 continue
             if visited[na][nb] == True:
+                print("         3.필터링된 na nb : ", na, " ", nb)
                 continue
             # 이동시 그래프 리스트에 해당 노드까지의 이동 거리를 입력
             graph[na][nb] = graph[a][b] + 1
