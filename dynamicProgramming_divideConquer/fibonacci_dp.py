a2cba5a3bae97afb1eb275b58eb9cff6ea0f1c61
awbwd4 <awbwd4@gmail.com>
2022-05-31 14:27:19 +0900

    22.05.31 DP_fibonacci

---
+++ dynamicProgramming_divideConquer/fibonacci_dp.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+
+    cache = [0 for _ in range(n + 1)]
+    cache[0] = 0
+    cache[1] = 1
+    for i in range(2, n + 1):
+        cache[i] = cache[i - 1] + cache[i - 2]
+    return cache[n]
+
+
+print(fibonacci(10))
