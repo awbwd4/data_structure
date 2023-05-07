0568f2235fe44e74c594c0aeb2af3d0a52a3ec62
awbwd4 <awbwd4@gmail.com>
2023-05-07 19:27:51 +0900

    23.05.07 dynamic programming

---
+++ dynamicProgramming_divideConquer/fibonacci_dp2.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+
+def fib_dp(num):
+    cache = [0 for i in range(num+1)]
+    cache[0] = 0
+    cache[1] = 1
+
+    for i in range(2,num+1):
+        cache[i] = cache[i-1]+cache[i-2]
+
+    return cache[num]
+
+print(fib_dp(n))
